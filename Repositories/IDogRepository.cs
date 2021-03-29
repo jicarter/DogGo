25a62e7eb4cb22391fea33568bc7fc9b90f55229
Jordan Carter <jordan.carter.0023@gmail.com>
2021-03-29 15:33:50 -0500

    started chapter 4

---
+++ Repositories/IDogRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using System.Collections.Generic;
+
+
+namespace DogGo.Repositories
+{
+    public class IDogRepository
+    {
+        List<Dog> GetAllDogs();
+        Dog GetDogById(int id);
+        void AddDog(Dog dog);
+        void UpdateDog(Dog dog);
+        void DeleteDog(int dogId);
+    }
+}
